16a6dc4cdfb299cd6ac2f4f9f3ea69741ad11476
Brian Garland <briangarlandk@gmail.com>
2021-11-05 19:21:22 -0500

    Current day is displayed

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// set the current day
+var today = moment().format("dddd, MMMM Do");
+// set the current hour
+var current = moment().format("H A");
+
+// displays current day in header
+$('#currentDay').text(today)
+
+$(function(){
+
+})
+
+
